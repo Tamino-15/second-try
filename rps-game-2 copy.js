dea29941558cbe4f872e37df2a8a2529959a2f4c
Tamino-15 <tamino150405@gmail.com>
2023-08-01 16:56:45 +0100

    finished

---
+++ rps-game-2 copy.js
@@ -0,0 +1,153 @@
+function getRandomItem(arr) {
+
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    const item = arr[randomIndex];
+
+    return item;
+}
+
+function computerPlay(){
+
+    const array = ["Rock", 'Paper', "Scissors"];
+
+    const result = getRandomItem(array);
+
+    return result;
+}
+
+
+let playerscore=0;
+let computerscore=0;
+let playedrounds=0;
+
+let container = document.getElementById('result').innerHTML;
+
+let container2 = document.getElementById('numberofrounds').innerHTML;
+
+let container3 = document.getElementById('computerscore').innerHTML;
+
+let container4 = document.getElementById('playerscore').innerHTML;
+
+
+function paperSelection(selection){
+
+    const paperbtn = document.querySelector('#paper').innerHTML;
+
+    const computer= computerPlay();
+
+    if (computer==paperbtn){
+
+        container = "Tie";
+        
+        playedrounds++;
+        container2 = "Rounds played: "+playedrounds;
+        
+    }
+    else if(computer=="Scissors"){
+
+        container = 'Computer wins';
+        
+        playedrounds++;
+        container2 =  "Rounds played: "+playedrounds;
+        
+        computerscore++;
+        container3 = "Computer: " +computerscore;
+    }
+    else{
+
+        container = 'You win';
+        
+        playedrounds++;
+        container2 = "Rounds played: "+playedrounds;
+   
+        playerscore++;
+        container4 = "Player: " +playerscore;
+    }
+}
+
+function scissorsSelection(selection){
+
+    const scissorsbtn = document.querySelector('#scissors').innerHTML;
+
+    const computer= computerPlay();
+
+    if (computer==scissorsbtn){
+         container = "Tie";
+        
+        playedrounds++;
+        container2 = "Rounds played: "+playedrounds;
+        
+    }
+    else if(computer=="Rock"){
+        
+        container ='Computer wins';
+        
+        playedrounds++;
+        container2 = "Rounds played: "+playedrounds;
+        
+        computerscore++;
+        container3 = "Computer: " +computerscore;
+    
+    }
+    else{
+        container ='You win';
+        
+        playedrounds++;
+        container2 = "Rounds played: "+playedrounds;
+   
+        playerscore++;
+        container4 ="Player: " +playerscore;
+    
+     }
+}
+
+function rockSelection(selection){
+    const rockbtn = document.querySelector('#rock').innerHTML;
+
+    const computer= computerPlay();
+
+    if (computer==rockbtn){
+         container = "Tie";
+        
+        playedrounds++;
+        container2 = "Rounds played: "+playedrounds;
+        
+    }
+    else if(computer=="Paper"){
+        
+        container ='Computer wins';
+        
+        playedrounds++;
+        container2 = "Rounds played: "+playedrounds;
+        
+        computerscore++;
+        container3 = "Computer: " +computerscore;
+    
+    }
+    else{
+        container ='You win';
+        
+        playedrounds++;
+        container2 = "Rounds played: "+playedrounds;
+   
+        playerscore++;
+        container4 ="Player: " +playerscore;
+    
+     }
+
+}
+
+function resetButton(){
+
+    container = 'Result of the round is...';
+
+    container2 = 'Rounds played: 0';
+
+    container3 = 'Computer: 0';
+
+    container4 = "Player: 0";
+
+
+}   
+
--- rps-game-2.js
+++ rps-game-2.js
@@ -1,25 +1,87 @@
 function getRandomItem(arr) {
-
     const randomIndex = Math.floor(Math.random() * arr.length);
-
     const item = arr[randomIndex];
-
     return item;
 }
 
-function computerPlay(){
-
-    const array = ["Rock", 'Paper', "Scissors","Rock", 'Paper', "Scissors"];
-
+function computerPlay() {
+    const array = ["Rock", 'Paper', "Scissors"];
     const result = getRandomItem(array);
-
     return result;
 }
 
-function playerSelection(selection){
+let playerscore = 0;
+let computerscore = 0;
+let playedrounds = 0;
 
-    let player = document.getElementById("options").innerHTML + selection;
+function tie() {
+    let container = document.getElementById('result').innerHTML = "Tie";
+    playedrounds++;
+    let container2 = document.getElementById('numberofrounds').innerHTML = "Rounds played: " + playedrounds;
+}
 
-    console.log(player);
- 
+function computerWin() {
+    let container = document.getElementById('result').innerHTML = 'Computer wins';
+    playedrounds++;
+    let container2 = document.getElementById('numberofrounds').innerHTML = "Rounds played: " + playedrounds;
+    computerscore++;
+    let container3 = document.getElementById('computerscore').innerHTML = "Computer: " + computerscore;
+}
+
+function playerWin() {
+    let container = document.getElementById('result').innerHTML = 'You win';
+    playedrounds++;
+    let container2 = document.getElementById('numberofrounds').innerHTML = "Rounds played: " + playedrounds;
+    playerscore++;
+    let container4 = document.getElementById('playerscore').innerHTML = "Player: " + playerscore;
+}
+
+
+function paperSelection(selection) {
+    const paperbtn = document.querySelector('#paper').innerHTML;
+    const computer = computerPlay();
+    if (computer == paperbtn) {
+        tie();
+    }
+    else if (computer == "Scissors") {
+        computerWin();
+    }
+    else {
+        playerWin();
+    }
+}
+
+function scissorsSelection(selection) {
+    const scissorsbtn = document.querySelector('#scissors').innerHTML;
+    const computer = computerPlay();
+    if (computer == scissorsbtn) {
+        tie();
+    }
+    else if (computer == "Rock") {
+        computerWin();
+    }
+    else {
+        playerWin();
+    }
+}
+
+function rockSelection(selection) {
+    const rockbtn = document.querySelector('#rock').innerHTML;
+    const computer = computerPlay();
+    if (computer == rockbtn) {
+        tie();
+    }
+    else if (computer == "Paper") {
+        computerWin();
+    }
+    else {
+        playerWin();
+    }
+}
+
+function resetButton() {
+    let container = document.getElementById('result').innerHTML = 'Result of the round is...';
+    let container2 = document.getElementById('numberofrounds').innerHTML = 'Rounds played: 0';
+    let container3 = document.getElementById('computerscore').innerHTML = 'Computer: 0';
+    let container4 = document.getElementById('playerscore').innerHTML = "Player: 0";
 }
